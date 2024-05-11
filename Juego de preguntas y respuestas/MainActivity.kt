a840c6ffe0aad3c23e0c67a398054ece56653d0d
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-05-11 08:48:04 -0500

    Create MainActivity.kt

---
+++ Juego de preguntas y respuestas/MainActivity.kt
@@ -0,0 +1,23 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, WelcomeFragment())
+                .commit()
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
